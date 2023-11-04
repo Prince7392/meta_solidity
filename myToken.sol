fab840b235ac327bb53050bb9779c0ad0c384f91
Prince7392 <90296102+Prince7392@users.noreply.github.com>
2023-11-04 21:56:44 +0530

    Create myToken.sol

---
+++ myToken.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract MyToken {
+
+    // public variables here
+    string public Token_name = "Sarvendra";
+    string public Token_abbr = "Singh";
+    uint public Total_supply = 0;
+
+    // mapping variable here
+    mapping(address => uint) public Balance;
+
+    // mint function
+    function mint(address _Addr, uint _Val) public {
+        Total_supply += _Val;
+        Balance[_Addr] += _Val;
+    }
+
+    // burn function
+    function burn(address _Addr, uint _Val) public {
+        if(Balance[_Addr] >= _Val) {
+            Total_supply -= _Val;
+            Balance[_Addr] -= _Val;
+    }
+    }
+}
